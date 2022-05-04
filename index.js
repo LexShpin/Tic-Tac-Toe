e3c76e4372f07135d1581baa4cc8bc874963463c
lexshpin <lexshpin@gmail.com>
2022-05-03 19:42:00 -0600

    Started planning out the logic

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('hello, world')
+const gameFields = document.querySelectorAll('.board-cell')
+
+const gameBoard = (function() {
+    let _board = []
+
+    gameFields.forEach(field => field.addEventListener('click', (e) => {
+        field.textContent = 'X'
+        _board.push('X')
+        console.log(_board)
+    }))
+
+    return {}
+})()
+
+const displayController = (function() {
+    
+
+    return {}
+})()
+
+const Player = option => {
+
+}
