5a259213efe1d25ebe9be124ce6b3c5f08892fb3
lexshpin <lexshpin@gmail.com>
2022-05-07 20:13:55 -0600

    Started writing out the logic

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const gameBoard = (function() {
     let _board = []
 
     gameFields.forEach(field => field.addEventListener('click', (e) => {
+        if (field.textContent != '') {
+            return
+        }
         field.textContent = 'X'
         _board.push('X')
         console.log(_board)
@@ -19,5 +22,11 @@ const displayController = (function() {
 })()
 
 const Player = option => {
+    this.option = option
 
+    const getOption = () => {
+        return option
+    }
+
+    return {getOption}
 }
