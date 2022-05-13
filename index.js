fd2f0183ffe0ba923b7e17b38f73401553a21b30
Alexander Shpin <@lexhpin@gmail.com>
2022-05-13 12:18:17 -0500

    Wrote out the logic for players to make moves one after another

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 const gameFields = document.querySelectorAll('.board-cell')
 
+const Player = option => {
+    this.option = option
+
+    const getOption = () => {
+        return option
+    }
+
+    return {getOption}
+}
+
 const gameBoard = (function() {
     let _board = []
+    const _player1 = Player('X')
+    const _player2 = Player('O')
+
+    let activePlayer = 'player_1'
 
     gameFields.forEach(field => field.addEventListener('click', (e) => {
         if (field.textContent != '') {
             return
         }
-        field.textContent = 'X'
-        _board.push('X')
+
+        if (activePlayer == 'player_1') {
+            field.textContent = _player1.getOption()
+            _board.push(_player1.getOption())
+            activePlayer = 'player_2'
+        } else if (activePlayer == 'player_2') {
+            field.textContent = _player2.getOption()
+            _board.push(_player2.getOption())
+            activePlayer = 'player_1'
+        }
+
         console.log(_board)
     }))
 
@@ -21,12 +44,3 @@ const displayController = (function() {
     return {}
 })()
 
-const Player = option => {
-    this.option = option
-
-    const getOption = () => {
-        return option
-    }
-
-    return {getOption}
-}
